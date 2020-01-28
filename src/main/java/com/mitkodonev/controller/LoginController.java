64e4c9acd8e3736e47029534dd6aa4e5d3e5dbd6
Rangel Stefanov <rangel.stefanov@gmail.com>
2020-01-28 13:09:21 +0100

    Many changes

--- src/main/java/com/mitkodonev/controller/LoginController.java
+++ src/main/java/com/mitkodonev/controller/LoginController.java
@@ -3,13 +3,7 @@ package com.mitkodonev.controller;
 import com.mitkodonev.model.LoginData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.validation.Valid;
 
 @Controller
 public class LoginController {
@@ -17,7 +11,7 @@ public class LoginController {
     @RequestMapping("/login")
     public String requestLoginForm(Model model){
         model.addAttribute("loginData", new LoginData());
-        return "/login";
+        return "login";
     }
 
     @RequestMapping("/logout-success")
